134053931405717a40466516f6499924c58f82a7
Mohamed Emara <medoemara104@gmail.com>
2025-05-15 21:14:49 +0300

    Setup project structure & install dependencies

---
+++ src/test/java/com/emara/SpringHotel/SpringHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emara.SpringHotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
