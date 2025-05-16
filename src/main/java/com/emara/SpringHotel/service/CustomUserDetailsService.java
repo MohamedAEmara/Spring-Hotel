ecab1dc1a3d2f9d2c1dd545e9786d335bd145c43
Mohamed Emara <medoemara104@gmail.com>
2025-05-16 18:56:42 +0300

    Implement CustomUserDetailsService class

---
+++ src/main/java/com/emara/SpringHotel/service/CustomUserDetailsService.java
@@ -0,0 +1,20 @@
+package com.emara.SpringHotel.service;
+
+import com.emara.SpringHotel.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return userRepository.findByEmail(email).orElseThrow(() -> new UsernameNotFoundException("Username/Email not found"));
+    }
+}
