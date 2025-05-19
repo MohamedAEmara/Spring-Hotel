a1fcc4265b7a0e97360d1ac78982e1aaebef51ad
Mohamed Emara <medoemara104@gmail.com>
2025-05-19 16:30:58 +0300

    Add getters-setters for LoginRequestDTO class

--- src/main/java/com/emara/SpringHotel/dto/LoginRequestDTO.java
+++ src/main/java/com/emara/SpringHotel/dto/LoginRequestDTO.java
@@ -9,6 +9,21 @@ public class LoginRequestDTO {
     @NotBlank(message = "password is required!")
     private String password;
 
+    public LoginRequestDTO() {}
 
+    public String getEmail() {
+        return email;
+    }
 
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
