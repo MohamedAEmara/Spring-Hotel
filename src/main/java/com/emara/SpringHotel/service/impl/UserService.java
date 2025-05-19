f424b7d64305494d31e45a56afdcd02ea07100c5
Mohamed Emara <medoemara104@gmail.com>
2025-05-20 00:18:52 +0300

    Implement User/Auth service

--- src/main/java/com/emara/SpringHotel/service/impl/UserService.java
+++ src/main/java/com/emara/SpringHotel/service/impl/UserService.java
@@ -11,9 +11,13 @@ import com.emara.SpringHotel.utils.JWTUtils;
 import com.emara.SpringHotel.utils.Utils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class UserService implements IUserService {
@@ -56,31 +60,137 @@ public class UserService implements IUserService {
 
     @Override
     public ResponseDTO loginUser(LoginRequestDTO loginRequest) {
-        return null;
+        ResponseDTO response = new ResponseDTO();
+
+        try {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginRequest.getEmail(), loginRequest.getPassword()));
+
+            var user = userRepository.findByEmail(loginRequest.getEmail()).orElseThrow(()-> new CustomException("User Not Found!"));
+            var token = jwtUtils.generateToken(user);
+
+            response.setStatusCode(200);
+            response.setToken(token);
+            response.setRole(user.getRole());
+            response.setExpirationTime("7 Days");
+            response.setMessage("User logged in successfully!");
+        } catch (CustomException ex) {
+            response.setStatusCode(404);
+            response.setMessage(ex.getMessage());
+        } catch (Exception ex) {
+            response.setStatusCode(500);
+            response.setMessage("Something went wrong: \n" + ex.getMessage());
+        }
+
+        return response;
     }
 
     @Override
     public ResponseDTO getUserById(String userId) {
-        return null;
+        ResponseDTO response = new ResponseDTO();
+
+        try {
+            User user = userRepository.findById(Long.valueOf(userId)).orElseThrow(() -> new CustomException("User Not Found!"));
+            UserDTO userDTO = Utils.mapUserEntityToUserDTO(user);
+
+            response.setStatusCode(200);
+            response.setMessage("User deleted successfully!");
+            response.setUser(userDTO);
+        } catch (CustomException ex) {
+            response.setStatusCode(404);
+            response.setMessage(ex.getMessage());
+        } catch (Exception ex) {
+            response.setStatusCode(500);
+            response.setMessage(ex.getMessage());
+        }
+
+        return response;
     }
 
     @Override
     public ResponseDTO getAllUsers() {
-        return null;
+        ResponseDTO response = new ResponseDTO();
+        try {
+            List<User> userList = userRepository.findAll();
+            List<UserDTO> userDTOList = new ArrayList<>();
+            userDTOList = Utils.mapUserListEntityToUserListDTO(userList);
+
+            response.setStatusCode(200);
+            response.setMessage("All users found!");
+            response.setUserList(userDTOList);
+        } catch (CustomException ex) {
+            response.setStatusCode(404);
+            response.setMessage(ex.getMessage());
+        } catch (Exception ex) {
+            response.setStatusCode(500);
+            response.setMessage("Something went wrong: \n" + ex.getMessage());
+        }
+
+        return response;
     }
 
     @Override
     public ResponseDTO getUserBookingHistory(String userId) {
-        return null;
+        ResponseDTO response = new ResponseDTO();
+
+        try {
+            User user = userRepository.findById(Long.valueOf(userId)).orElseThrow(() -> new CustomException("User Not Found!"));
+            UserDTO userDTO = Utils.mapUserEntityToUserDTO(user);
+
+            response.setStatusCode(200);
+            response.setMessage("User booking history successfully!");
+            response.setUser(userDTO);
+        } catch (CustomException ex) {
+            response.setStatusCode(404);
+            response.setMessage(ex.getMessage());
+        } catch (Exception ex) {
+            response.setStatusCode(500);
+            response.setMessage(ex.getMessage());
+        }
+
+        return response;
     }
 
     @Override
     public ResponseDTO deleteUser(String userId) {
-        return null;
+        ResponseDTO response = new ResponseDTO();
+
+        try {
+            // We use this line before deletion to throw an error if the user_id is not correct.
+            userRepository.findById(Long.valueOf(userId)).orElseThrow(() -> new CustomException("User Not Found!"));
+            userRepository.deleteById(Long.valueOf(userId));
+
+            response.setStatusCode(200);
+            response.setMessage("User deleted successfully!");
+        } catch (CustomException ex) {
+            response.setStatusCode(404);
+            response.setMessage(ex.getMessage());
+        } catch (Exception ex) {
+            response.setStatusCode(500);
+            response.setMessage(ex.getMessage());
+        }
+
+        return response;
     }
 
     @Override
-    public ResponseDTO getMyInfo(String userId) {
-        return null;
+    public ResponseDTO getMyInfo(String email) {
+        ResponseDTO response = new ResponseDTO();
+
+        try {
+            User user = userRepository.findByEmail(email).orElseThrow(() -> new CustomException("User Not Found!"));
+            UserDTO userDTO = Utils.mapUserEntityToUserDTO(user);
+
+            response.setStatusCode(200);
+            response.setMessage("User info successfully!");
+            response.setUser(userDTO);
+        } catch (CustomException ex) {
+            response.setStatusCode(404);
+            response.setMessage(ex.getMessage());
+        } catch (Exception ex) {
+            response.setStatusCode(500);
+            response.setMessage(ex.getMessage());
+        }
+
+        return response;
     }
 }
